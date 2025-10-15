f061356e4c821c18fdfec66d9f76e001168f92ba
sekharreddy <sekharreddy1515@gmail.com>
2025-10-15 06:56:25 +0530

    Implementation of code for printing zero even and odd

---
+++ javasamples/src/multiThreading/src/dsasamples/PrintZeroEvenOdd.java
@@ -0,0 +1,120 @@
+package dsasamples;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.IntConsumer;
+
+public class PrintZeroEvenOdd {
+    public static void main(String[] args) {
+        ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(10);
+        Thread t1 = new Thread(() -> {
+            try {
+                zeroEvenOdd.zero(System.out::print);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                zeroEvenOdd.even(System.out::print);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t3 = new Thread(() -> {
+            try {
+                zeroEvenOdd.odd(System.out::print);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t3.start();
+        t2.start();
+        t1.start();
+    }
+}
+
+class ZeroEvenOdd {
+    private int n;
+    private AtomicInteger i;
+    private ReentrantLock lock;
+    private Condition evenCondition;
+    private Condition oddCondition;
+    private Condition zeroCondition;
+    private boolean printEvenOddValues; // false -> zero's turn, true -> even/odd's turn
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+        i = new AtomicInteger(1); // start from 1, not 0
+        lock = new ReentrantLock();
+        evenCondition = lock.newCondition();
+        oddCondition = lock.newCondition();
+        zeroCondition = lock.newCondition();
+        printEvenOddValues = false;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int count = 0; count < n; count++) { // print zero n times
+            lock.lock();
+            try {
+                while (printEvenOddValues) {
+                    zeroCondition.await();
+                }
+                printNumber.accept(0);
+                printEvenOddValues = true;
+
+                // signal either odd or even thread depending on current number
+                if (i.get() % 2 == 0) {
+                    evenCondition.signal();
+                } else {
+                    oddCondition.signal();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            lock.lock();
+            try {
+                while (!printEvenOddValues || i.get() % 2 != 0) {
+                    if (i.get() > n) return;
+                    evenCondition.await();
+                }
+                if (i.get() > n) return;
+
+                printNumber.accept(i.getAndIncrement());
+                printEvenOddValues = false;
+                zeroCondition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while (true) {
+            lock.lock();
+            try {
+                while (!printEvenOddValues || i.get() % 2 == 0) {
+                    if (i.get() > n) return;
+                    oddCondition.await();
+                }
+                if (i.get() > n) return;
+
+                printNumber.accept(i.getAndIncrement());
+                printEvenOddValues = false;
+                zeroCondition.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
+
