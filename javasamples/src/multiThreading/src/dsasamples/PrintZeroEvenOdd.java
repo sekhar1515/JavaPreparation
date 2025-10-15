7b62db5fce368809c0ff28a55671af82b6328d9e
sekharreddy <sekharreddy1515@gmail.com>
2025-10-15 07:06:21 +0530

    Implementation of code for printing zero even and odd using semaphores

--- javasamples/src/multiThreading/src/dsasamples/PrintZeroEvenOdd.java
+++ javasamples/src/multiThreading/src/dsasamples/PrintZeroEvenOdd.java
@@ -1,5 +1,6 @@
 package dsasamples;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -114,6 +115,47 @@ class ZeroEvenOdd {
             } finally {
                 lock.unlock();
             }
+        }
+    }
+}
+class ZeroEvenOddSemaphore {
+    private int n;
+    private Semaphore zeroSemaphore;
+    private Semaphore evenSemaphore;
+    private Semaphore oddSemaphore;
+    public ZeroEvenOddSemaphore(int n) {
+        this.n = n;
+        zeroSemaphore = new Semaphore(1);
+        evenSemaphore = new Semaphore(0);
+        oddSemaphore = new Semaphore(0);
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 1; i <= n; i++){
+            zeroSemaphore.acquire();
+            printNumber.accept(0);
+            if(i%2 == 0){
+                evenSemaphore.release();
+            }else{
+                oddSemaphore.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 2; i <= n ; i+=2){
+            evenSemaphore.acquire();
+            printNumber.accept(i);
+            zeroSemaphore.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 1; i <= n ; i+=2){
+            oddSemaphore.acquire();
+            printNumber.accept(i);
+            zeroSemaphore.release();
         }
     }
 }
